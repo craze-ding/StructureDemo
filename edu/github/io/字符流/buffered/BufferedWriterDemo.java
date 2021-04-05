f7963852e07f8534ee31827a3bf28e352e89f56b
craze-ding <dzjcrae12121@gmail.com>
2021-04-05 23:31:24 +0800

    缓冲区

---
+++ edu/github/io/字符流/buffered/BufferedWriterDemo.java
@@ -0,0 +1,26 @@
+/*
+ * @Descripttion: 
+ * @version: X3版本
+ * @Author: 丁志军
+ * @Date: 2021-04-05 18:45:04
+ */
+package buffered;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BufferedWriterDemo {
+    public static void main(String[] args) throws IOException {
+        FileWriter fw=new FileWriter("demo.txt");
+        BufferedWriter bw=new BufferedWriter(fw);
+        bw.write("abcdef");
+        bw.newLine();
+        bw.write("haha");
+        bw.flush();
+        bw.close();
+       
+        
+    }
+    
+}
