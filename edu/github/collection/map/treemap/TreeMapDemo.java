104d52f6c52727bbd434d1743f65b236d3c52cab
craze-ding <dzjcrae12121@gmail.com>
2021-04-06 22:30:27 +0800

    treemap应用

---
+++ edu/github/collection/map/treemap/TreeMapDemo.java
@@ -0,0 +1,37 @@
+/*
+ * @Descripttion: 
+ * @version: X3版本
+ * @Author: 丁志军
+ * @Date: 2021-04-02 18:47:00
+ */
+package edu.github.collection.map.treemap;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.Map.Entry;
+
+import edu.github.bin.Student;
+import edu.github.compartor.CompartorByname;
+
+public class TreeMapDemo {
+    public static void main(String[] args) {
+        TreeMap<Student,String> tm=new TreeMap<Student,String>(new CompartorByname());
+        method(tm);
+    }
+    private static void method(TreeMap<Student,String> tm ) {
+        tm.put(new Student("blisi", 22), "北京");
+        tm.put(new Student("azhangsang", 32), "上海");
+        tm.put(new Student("kwangwu", 45), "广州");
+        tm.put(new Student("craze", 18), "成都");
+        tm.put(new Student("rzhangsang", 32), "深圳");//hashcode(),equals()判断依据,equals()返回真不存;hashmap保证键唯一,前面的value被覆盖
+        Set<Entry<Student,String>> s=tm.entrySet();
+        Iterator<Entry<Student,String>> it=s.iterator();
+        while(it.hasNext()){
+            Map.Entry<Student,String> stu=it.next();
+            System.out.println(stu.getKey()+":"+stu.getValue());//hash无序
+        }
+    
+    }
+}
